969693657bc173aaf8d5c124c28f41df17482d89
edita <edignot@gmail.com>
2024-01-09 15:43:18 -0700

    contentful setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
     siteMetadata: {
         title: `gatsby-test-1`,
@@ -17,6 +22,13 @@ module.exports = {
                 path: './src/images/',
             },
             __key: 'images',
+        },
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: `7l2w9wi3tbdy`,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
         },
     ],
 }
