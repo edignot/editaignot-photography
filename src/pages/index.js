13a7d3453c09266c0122deba9d80f5cce94e134e
edita <edignot@gmail.com>
2024-01-09 19:17:37 -0700

    display cms image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,41 @@
 import * as React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
-    return <main>Test</main>
+    const { allContentfulImageEntry } = useStaticQuery(graphql`
+        query {
+            allContentfulImageEntry {
+                edges {
+                    node {
+                        id
+                        type
+                        image {
+                            gatsbyImageData(
+                                width: 1024
+                                placeholder: BLURRED
+                                formats: [AUTO, WEBP, AVIF]
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    const image = getImage(allContentfulImageEntry.edges[0].node.image)
+
+    return (
+        <main>
+            <StaticImage
+                src='../images/icon.png'
+                alt='logo'
+                width={50}
+                height={50}
+            />
+            <GatsbyImage image={image} alt='test' />
+        </main>
+    )
 }
 
 export default IndexPage
