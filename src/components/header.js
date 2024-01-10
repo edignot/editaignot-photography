f78f00c91750456ceadbd7fd34ac5168337564ba
edita <edignot@gmail.com>
2024-01-10 14:19:06 -0700

    layout template update

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Nav from './nav'
+import { header, h1 } from './header.module.css'
 
 const Header = ({ title }) => {
     return (
-        <header>
-            <h1>{title}</h1>
+        <header className={header}>
+            <Link to={'/'}>
+                <h1 className={h1}>{title}</h1>
+            </Link>
             <Nav />
         </header>
     )
